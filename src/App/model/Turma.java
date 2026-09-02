77dce4d1825746bb802d85ba15ad6bea07519c98
Leximíbel Langa <langaleximibel@gmail.com>
2026-09-02 20:33:35 +0200

    feat: implementar classe de turma

--- src/App/model/Turma.java
+++ src/App/model/Turma.java
@@ -1,5 +1,54 @@
 package App.model;
 
 public class Turma {
+    private int id;
+    private String nome;
+    private double valorMensalidade;
     
+    // construtores
+    public Turma() {
+    }
+
+    public Turma(String nome, double valorMensalidade) {
+        this.nome = nome;
+        this.valorMensalidade = valorMensalidade;
+    }
+
+    public Turma(int id, String nome, double valorMensalidade) {
+        this.id = id;
+        this.nome = nome;
+        this.valorMensalidade = valorMensalidade;
+    }
+    
+    // getters e setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValorMensalidade() {
+        return valorMensalidade;
+    }
+
+    public void setValorMensalidade(double valorMensalidade) {
+        this.valorMensalidade = valorMensalidade;
+    }
+
+    // util para exibir no ComboBox
+    @Override
+    public String toString() {
+        return "Turma: " + nome;
+    }
 }
+
