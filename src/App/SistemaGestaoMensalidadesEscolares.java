e4cfd850cec2bf89a703ac9c88331a8a41e79235
Leximíbel Langa <langaleximibel@gmail.com>
2026-09-02 19:48:16 +0200

    feat: implementar conexao e testar na main

--- src/App/SistemaGestaoMensalidadesEscolares.java
+++ src/App/SistemaGestaoMensalidadesEscolares.java
@@ -1,8 +1,13 @@
 package App;
 
+import App.util.ConexaoBD;
+
 public class SistemaGestaoMensalidadesEscolares {
 
     public static void main(String[] args) {
         System.out.println("Testando a aplicacao.");
+        
+        // testar conexao
+        System.out.println(ConexaoBD.getConexao());
     }
 }
