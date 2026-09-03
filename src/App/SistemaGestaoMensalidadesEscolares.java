b3d003187d77fc4a7e8e84cc4b762239a415138f
Leximíbel Langa <langaleximibel@gmail.com>
2026-09-03 15:59:40 +0200

    test: ajustes de formatacao no arquivo main, para testes

--- src/App/SistemaGestaoMensalidadesEscolares.java
+++ src/App/SistemaGestaoMensalidadesEscolares.java
@@ -10,10 +10,10 @@ import java.time.LocalDate;
 public class SistemaGestaoMensalidadesEscolares {
 
     public static void main(String[] args) {
-        System.out.println("Testando a aplicacao.");
-        
-        System.out.println("======= Testar conexao =====");
-        System.out.println(ConexaoBD.getConexao());
+//        System.out.println("Testando a aplicacao.");
+//        
+//        System.out.println("======= Testar conexao =====");
+//        System.out.println(ConexaoBD.getConexao());
         
 //        System.out.println("\n\n");
 //        System.out.println("====== TURMA =========");
@@ -21,16 +21,16 @@ public class SistemaGestaoMensalidadesEscolares {
 //        Turma t2 = new Turma("GESTAO", 5700);
 //        Turma t3 = new Turma("DIREITO", 5900);
 //        Turma t4 = new Turma("CONTABILIDADE", 5800);
-        TurmaDAO tBD = new TurmaDAO();
+//        TurmaDAO tBD = new TurmaDAO();
 //        tBD.inserir(t1);
 //        tBD.inserir(t2);
 //        tBD.inserir(t3);
 //        tBD.inserir(t4);
-        
-        
-        System.out.println("\n\n");
-        System.out.println("====== ALUNO =========");
-        AlunoDAO aBD = new AlunoDAO();
+
+//        System.out.println("\n\n");
+//        System.out.println("====== ALUNO =========");
+//        AlunoDAO aBD = new AlunoDAO();
+
         // turma: INFORMATICA
 //        Aluno a1 = new Aluno("Leximibel", "inf-01", "Fatima & Longuinho", "857921043", tBD.buscarPorId(2), LocalDate.now());
 //        Aluno a2 = new Aluno("Antonio", "inf-02", "Maria & Mucavele", "870000000", tBD.buscarPorId(2), LocalDate.now());
@@ -47,22 +47,21 @@ public class SistemaGestaoMensalidadesEscolares {
 //        aBD.inserir(a5);
 //        aBD.inserir(a6);
 
-        // turma: GESTAO
-        Aluno a7 = new Aluno("Sebastiao", "g-01", "Amelia & Sergio", "854434620", tBD.buscarPorId(3), LocalDate.now());
-        Aluno a8 = new Aluno("Gina", "g-02", "Eliana & Paulo", "877725729", tBD.buscarPorId(3), LocalDate.now());
-        Aluno a9 = new Aluno("Issufo", "g-03", "Hairate & Mohamad", "851112666", tBD.buscarPorId(3), LocalDate.now());
-        aBD.inserir(a7);
-        aBD.inserir(a8);
-        aBD.inserir(a9);
-        
-        // turma: CONTABILIDADE
-        Aluno a10 = new Aluno("Zito", "c-01", "Ana & Lopes", "857756622", tBD.buscarPorId(5), LocalDate.now());
-        Aluno a11 = new Aluno("Mariana", "c-02", "Sonia & Joao", "877728721", tBD.buscarPorId(5), LocalDate.now());
-        Aluno a12 = new Aluno("Santos", "c-03", "Cinthia & Shelton", "851230066", tBD.buscarPorId(5), LocalDate.now());
-        aBD.inserir(a10);
-        aBD.inserir(a11);
-        aBD.inserir(a12);
+          // turma: GESTAO
+//        Aluno a7 = new Aluno("Sebastiao", "g-01", "Amelia & Sergio", "854434620", tBD.buscarPorId(3), LocalDate.now());
+//        Aluno a8 = new Aluno("Gina", "g-02", "Eliana & Paulo", "877725729", tBD.buscarPorId(3), LocalDate.now());
+//        Aluno a9 = new Aluno("Issufo", "g-03", "Hairate & Mohamad", "851112666", tBD.buscarPorId(3), LocalDate.now());
+//        aBD.inserir(a7);
+//        aBD.inserir(a8);
+//        aBD.inserir(a9);
+//        
+//        // turma: CONTABILIDADE
+//        Aluno a10 = new Aluno("Zito", "c-01", "Ana & Lopes", "857756622", tBD.buscarPorId(5), LocalDate.now());
+//        Aluno a11 = new Aluno("Mariana", "c-02", "Sonia & Joao", "877728721", tBD.buscarPorId(5), LocalDate.now());
+//        Aluno a12 = new Aluno("Santos", "c-03", "Cinthia & Shelton", "851230066", tBD.buscarPorId(5), LocalDate.now());
+//        aBD.inserir(a10);
+//        aBD.inserir(a11);
+//        aBD.inserir(a12);
 
-        
     }
 }
