ef7f0ed405acb4779e7389e59752392a71419d88
Leximíbel Langa <langaleximibel@gmail.com>
2026-09-03 14:19:01 +0200

    test: testar TurmaDAO (comunicao entre classe turma e BD)

--- src/App/SistemaGestaoMensalidadesEscolares.java
+++ src/App/SistemaGestaoMensalidadesEscolares.java
@@ -1,5 +1,7 @@
 package App;
 
+import App.dao.TurmaDAO;
+import App.model.Turma;
 import App.util.ConexaoBD;
 
 public class SistemaGestaoMensalidadesEscolares {
@@ -7,7 +9,20 @@ public class SistemaGestaoMensalidadesEscolares {
     public static void main(String[] args) {
         System.out.println("Testando a aplicacao.");
         
-        // testar conexao
+        System.out.println("======= Testar conexao =====");
         System.out.println(ConexaoBD.getConexao());
+        
+        System.out.println("\n\n");
+        System.out.println("====== INSERIR TURMA =========");
+        Turma t1 = new Turma("INFORMATICA", 6300);
+        Turma t2 = new Turma("GESTAO", 5700);
+        Turma t3 = new Turma("DIREITO", 5900);
+        Turma t4 = new Turma("CONTABILIDADE", 5800);
+        TurmaDAO tBD = new TurmaDAO();
+        tBD.inserir(t1);
+        tBD.inserir(t2);
+        tBD.inserir(t3);
+        tBD.inserir(t4);
+        
     }
 }
