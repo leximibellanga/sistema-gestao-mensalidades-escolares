4435b8615744210e2143feb2c2027b2a351ef70b
Leximíbel Langa <langaleximibel@gmail.com>
2026-09-03 22:35:45 +0200

    refactor: eliminar todos testes realizados

--- src/App/SistemaGestaoMensalidadesEscolares.java
+++ src/App/SistemaGestaoMensalidadesEscolares.java
@@ -1,67 +1,18 @@
 package App;
 
-import App.dao.AlunoDAO;
-import App.dao.TurmaDAO;
-import App.model.Aluno;
-import App.model.Turma;
-import App.util.ConexaoBD;
+import App.dao.MensalidadeDAO;
 import java.time.LocalDate;
+import java.time.Month;
 
 public class SistemaGestaoMensalidadesEscolares {
 
     public static void main(String[] args) {
-//        System.out.println("Testando a aplicacao.");
-//        
-//        System.out.println("======= Testar conexao =====");
-//        System.out.println(ConexaoBD.getConexao());
+        System.out.println("SGME");
         
-//        System.out.println("\n\n");
-//        System.out.println("====== TURMA =========");
-//        Turma t1 = new Turma("INFORMATICA", 6300);
-//        Turma t2 = new Turma("GESTAO", 5700);
-//        Turma t3 = new Turma("DIREITO", 5900);
-//        Turma t4 = new Turma("CONTABILIDADE", 5800);
-//        TurmaDAO tBD = new TurmaDAO();
-//        tBD.inserir(t1);
-//        tBD.inserir(t2);
-//        tBD.inserir(t3);
-//        tBD.inserir(t4);
-
-//        System.out.println("\n\n");
-//        System.out.println("====== ALUNO =========");
-//        AlunoDAO aBD = new AlunoDAO();
-
-        // turma: INFORMATICA
-//        Aluno a1 = new Aluno("Leximibel", "inf-01", "Fatima & Longuinho", "857921043", tBD.buscarPorId(2), LocalDate.now());
-//        Aluno a2 = new Aluno("Antonio", "inf-02", "Maria & Mucavele", "870000000", tBD.buscarPorId(2), LocalDate.now());
-//        Aluno a3 = new Aluno("Humeid", "inf-03", "Angelica & Modan", "851111111", tBD.buscarPorId(2), LocalDate.now());
-//        aBD.inserir(a1);
-//        aBD.inserir(a2);
-//        aBD.inserir(a3);
-
-        // turma: DIREITO
-//        Aluno a4 = new Aluno("Pedro", "d-01", "Sonia & Gerson", "857756620", tBD.buscarPorId(4), LocalDate.now());
-//        Aluno a5 = new Aluno("Melacia", "d-02", "Sonia & Gerson", "877728729", tBD.buscarPorId(4), LocalDate.now());
-//        Aluno a6 = new Aluno("Zinneyd", "d-03", "Leila & Moises", "851232666", tBD.buscarPorId(4), LocalDate.now());
-//        aBD.inserir(a4);
-//        aBD.inserir(a5);
-//        aBD.inserir(a6);
-
-          // turma: GESTAO
-//        Aluno a7 = new Aluno("Sebastiao", "g-01", "Amelia & Sergio", "854434620", tBD.buscarPorId(3), LocalDate.now());
-//        Aluno a8 = new Aluno("Gina", "g-02", "Eliana & Paulo", "877725729", tBD.buscarPorId(3), LocalDate.now());
-//        Aluno a9 = new Aluno("Issufo", "g-03", "Hairate & Mohamad", "851112666", tBD.buscarPorId(3), LocalDate.now());
-//        aBD.inserir(a7);
-//        aBD.inserir(a8);
-//        aBD.inserir(a9);
-//        
-//        // turma: CONTABILIDADE
-//        Aluno a10 = new Aluno("Zito", "c-01", "Ana & Lopes", "857756622", tBD.buscarPorId(5), LocalDate.now());
-//        Aluno a11 = new Aluno("Mariana", "c-02", "Sonia & Joao", "877728721", tBD.buscarPorId(5), LocalDate.now());
-//        Aluno a12 = new Aluno("Santos", "c-03", "Cinthia & Shelton", "851230066", tBD.buscarPorId(5), LocalDate.now());
-//        aBD.inserir(a10);
-//        aBD.inserir(a11);
-//        aBD.inserir(a12);
-
+        MensalidadeDAO bdMensal = new MensalidadeDAO();
+        System.out.println("\n\nEm atraso: \t" + bdMensal.listarEmAtraso());
+        System.out.println("\n\nPendentes com data vencida: \t" + bdMensal.listarPendentesComDataVencida());
+        System.out.println("\n\nEm Por aluno [5]: \t" + bdMensal.listarPorAluno(6));
+        System.out.println("\n\nPor mes [agosto]: \t" + bdMensal.listarPorMesReferencia(LocalDate.of(2026, Month.MARCH, 1)));
     }
 }
