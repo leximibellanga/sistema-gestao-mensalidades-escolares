3cd045ae736f9a8cecc5279e5bac1a55d6377199
Leximíbel Langa <langaleximibel@gmail.com>
2026-09-18 21:14:48 +0200

    test: testar a funcao count da DashboardDAO

--- src/App/SistemaGestaoMensalidadesEscolares.java
+++ src/App/SistemaGestaoMensalidadesEscolares.java
@@ -1,8 +1,12 @@
 package App;
 
+import App.dao.DashboardDAO;
+
 public class SistemaGestaoMensalidadesEscolares {
 
     public static void main(String[] args) {
         System.out.println("SGME");
+        DashboardDAO dao = new DashboardDAO();
+        System.out.println(dao.countAlunos());
     }
 }
